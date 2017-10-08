64226ebe4bd687c43e4df4ff5f773b47947d8fa7
Gustavo Coronel <gcoronelc@gmail.com>
2017-10-08 15:39:26 -0500

    Sem 06

--- Sem06/VentaTalleres/src/pe/egcc/ventatalleres/VentaTalleres.java
+++ Sem06/VentaTalleres/src/pe/egcc/ventatalleres/VentaTalleres.java
@@ -1,9 +1,11 @@
 package pe.egcc.ventatalleres;
 
+import pe.egcc.ventatalleres.view.VentaView;
+
 public class VentaTalleres {
 
   public static void main(String[] args) {
-    
+    VentaView.main(args);
   }
   
 }
